df48db719600864772b861723bc8b3994252e764
esme518 <esme518@gmail.com>
2024-04-05 16:47:47 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/mosdns
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
+git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
