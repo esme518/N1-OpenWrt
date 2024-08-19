670f4c4b9f101ebec671dac126e47ceee5bc6714
esme518 <esme518@gmail.com>
2024-08-19 22:25:43 +0900

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Remove packages
+rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/net/v2ray-geodata
 rm -rf feeds/packages/net/mosdns
 
 # Add packages
+git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
